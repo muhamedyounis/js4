fae18e6548153aeaba99d3c4567a5108b2b07136
muhamedyounis <muhamad.ashraf.younis@gmail.com>
2024-02-08 17:43:08 +0200

    paths

--- js/script.js
+++ js/script.js
@@ -136,14 +136,14 @@ if (window.location.pathname == "/js4/home.html") {
   console.log("home");
   if (sessionStorage.getItem("userLoggedIn") == null) {
     document.getElementById("notLoggedIn").style.display = "block";
-    var logoutBtn = document.getElementById("logoutBtn");
-    logoutBtn.addEventListener("click", () => {
-      sessionStorage.removeItem("userLoggedIn");
-      window.location.href = "/js4/index.html";
-    });
   } else {
     document.getElementById("loggedIn").style.display = "block";
     document.getElementById("userName").innerHTML =
       sessionStorage.getItem("userLoggedIn");
   }
+  var logoutBtn = document.getElementById("logoutBtn");
+  logoutBtn.addEventListener("click", () => {
+    sessionStorage.removeItem("userLoggedIn");
+    window.location.href = "/js4/index.html";
+  });
 }
